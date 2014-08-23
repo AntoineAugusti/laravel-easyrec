3399e662416b99864dd6900f30d1da3ba5a0cd2e
Antoine Augusti <antoine@augusti.fr>
2014-08-23 10:12:40 +0200

    Add tests for buy and rate

--- tests/Antoineaugusti/LaravelEasyrec/EasyrecTest.php
+++ tests/Antoineaugusti/LaravelEasyrec/EasyrecTest.php
@@ -18,6 +18,7 @@ class EasryrecTest extends PHPUnit_Framework_TestCase {
 	const ITEM_ID = 1337;
 	const ITEM_DESCRIPTION = "mock-description";
 	const ITEM_URL = "mock-url";
+	const RATING_NOTE = 5;
 
 
 	public function setUp()
@@ -47,6 +48,10 @@ class EasryrecTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('mock-url/api/1.0/json/', $this->easyrec->getBaseURL());
 	}
 
+	/*
+	* ACTIONS 
+	* --------------------
+	*/
 	public function testView()
 	{
 		$this->easyrec->view(self::ITEM_ID, self::ITEM_DESCRIPTION, self::ITEM_URL);
@@ -64,5 +69,51 @@ class EasryrecTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($queryParams["itemid"], self::ITEM_ID);
 		$this->assertEquals($queryParams["itemdescription"], self::ITEM_DESCRIPTION);
 		$this->assertEquals($queryParams["itemurl"], self::ITEM_URL);
+	}
+
+	public function testBuy()
+	{
+		$this->easyrec->buy(self::ITEM_ID, self::ITEM_DESCRIPTION, self::ITEM_URL);
+
+		// Test required keys
+		$requiredKeys = ['itemid', 'itemdescription', 'itemurl', 'sessionid'];
+		$queryParams = $this->easyrec->getQueryParams();
+		foreach ($requiredKeys as $key)
+			$this->assertArrayHasKey($key, $queryParams);
+
+		// Test values in the request
+		$this->assertEquals($queryParams["apikey"], "mock-key");
+		$this->assertEquals($queryParams["tenantid"], "mock-tenant");
+		$this->assertEquals($queryParams["sessionid"], 42);
+		$this->assertEquals($queryParams["itemid"], self::ITEM_ID);
+		$this->assertEquals($queryParams["itemdescription"], self::ITEM_DESCRIPTION);
+		$this->assertEquals($queryParams["itemurl"], self::ITEM_URL);
+	}
+
+	public function testRate()
+	{
+		$this->easyrec->rate(self::ITEM_ID, self::RATING_NOTE, self::ITEM_DESCRIPTION, self::ITEM_URL);
+
+		// Test required keys
+		$requiredKeys = ['itemid', 'ratingvalue', 'itemdescription', 'itemurl', 'sessionid'];
+		$queryParams = $this->easyrec->getQueryParams();
+		foreach ($requiredKeys as $key)
+			$this->assertArrayHasKey($key, $queryParams);
+
+		// Test values in the request
+		$this->assertEquals($queryParams["apikey"], "mock-key");
+		$this->assertEquals($queryParams["tenantid"], "mock-tenant");
+		$this->assertEquals($queryParams["sessionid"], 42);
+		$this->assertEquals($queryParams["itemid"], self::ITEM_ID);
+		$this->assertEquals($queryParams["ratingvalue"], self::RATING_NOTE);
+		$this->assertEquals($queryParams["itemdescription"], self::ITEM_DESCRIPTION);
+		$this->assertEquals($queryParams["itemurl"], self::ITEM_URL);
+	}
+
+    public function testRateException()
+	{
+		// Giving a string instead of a note should give an exception
+		$this->setExpectedException('InvalidArgumentException');
+		$this->easyrec->rate(self::ITEM_ID, "not a note", self::ITEM_DESCRIPTION, self::ITEM_URL);
 	}
 }
